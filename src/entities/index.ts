2b0d9e8476bc96c6ce75f67c3e346d6897f5bdb1
mcpower <mcpower@users.noreply.github.com>
2022-02-27 10:35:07 +1100

    add snowflake note

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,1 +1,6 @@
+// NOTE: Every entity which uses a Discord snowflake as an sqlite signed
+// int8/bigint will not work in September 2084.
+// Twitter snowflakes will have the same problem in July 2080, but that is WAI
+// as Twitter snowflakes are intended to be 63 bits.
+// Solution: store them in the db as (num - 2**63) in 2080.
 export * from "./Post";
