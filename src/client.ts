a4943d71ff041166e701d79d8c198d024669aea3
mcpower <mcpower@users.noreply.github.com>
2022-02-26 16:42:52 +1100

    more hello world

--- src/client.ts
+++ src/client.ts
@@ -1,15 +1,29 @@
-import { Client, Intents } from "discord.js";
+import process from "process";
+import { Client, Intents, Message } from "discord.js";
 import { token } from "./config";
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-
-// When the client is ready, run this code (only once)
-client.once("ready", () => {
-  console.log("Ready!");
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
+// When the client is ready, run this code (only once)
+client.once("ready", (client) => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on("messageCreate", (message: Message) => {
+  const { member, content } = message;
+  console.log(`Received "${content}" from ${member?.user.tag}`);
+});
+
+function ignoreAllErrors() {
+  client.on("error", console.error);
+  process.on("unhandledRejection", console.error);
+}
+
 export async function run(): Promise<void> {
+  ignoreAllErrors();
   // Login to Discord with your client's token
   await client.login(token);
 }
