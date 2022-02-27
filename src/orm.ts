24bc7574bfcf113bb1a1a5101ca5aa1b9ba9fbd2
mcpower <mcpower@users.noreply.github.com>
2022-02-27 19:50:21 +1100

    remove orm testing

--- src/orm.ts
+++ src/orm.ts
@@ -1,15 +1,10 @@
 import { MikroORM } from "@mikro-orm/core";
 import type { SqliteDriver } from "@mikro-orm/sqlite"; // or any other driver package
-import assert from "assert";
-import { Link } from "./entities";
 import config from "./orm.config";
 
-(async () => {
-  const orm = await MikroORM.init<SqliteDriver>(config);
-  const em = orm.em.fork();
-  const test = Link.fromUrl("twitter.com");
-  console.log(`${test.url} adding as id ${test.id}`);
-  await em.persistAndFlush(test);
-  assert(test.hasId());
-  console.log(`${test.url} added as id ${test.id}`);
-})();
+const ormPromise = MikroORM.init<SqliteDriver>(config);
+
+export async function getEm() {
+  const orm = await ormPromise;
+  return orm.em.fork();
+}
