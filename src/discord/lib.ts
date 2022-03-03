fe3596cb6f3a3fcb3038a78c29eb4cf71ea4ce04
mcpower <mcpower@users.noreply.github.com>
2022-03-03 21:31:32 +1100

    reverse getAllMessages

--- src/discord/lib.ts
+++ src/discord/lib.ts
@@ -8,24 +8,31 @@ import { compareBigints } from "../utils";
 
 const MAX_MESSAGES_PER_FETCH = 100;
 
-export async function* getAllMessages(channel: BaseGuildTextChannel) {
-  let lastSeen: Snowflake | undefined = undefined;
+/**
+ * Gets all messages since a given post.
+ * @param channel The channel to fetch messages from.
+ * @param after A Discord post snowflake.
+ */
+export async function* getAllMessages(
+  channel: BaseGuildTextChannel,
+  after = "0"
+) {
   while (true) {
     const messages: Collection<Snowflake, Message> =
       await channel.messages.fetch({
         limit: MAX_MESSAGES_PER_FETCH,
-        before: lastSeen,
+        after,
       });
     // Discord doesn't guarantee any order for these messages...
     // https://discord.com/developers/docs/resources/channel#get-channel-messages
-    // Sort from highest ID to lowest.
+    // Sort from lowest ID to highest.
     messages.sort((_a, _b, a, b) => {
-      return -compareBigints(a, b);
+      return compareBigints(a, b);
     });
     yield* messages.values();
-    const wtf = messages.last();
-    if (wtf) {
-      lastSeen = wtf.id;
+    const lastMessage = messages.last();
+    if (lastMessage) {
+      after = lastMessage.id;
     } else {
       break;
     }
