f79fe8bed519327e08f0c7b208f6b82bfb3c3627
mcpower <mcpower@users.noreply.github.com>
2022-03-05 20:49:19 +1100

    update packages (use better-sqlite, ts-node)

--- src/orm.config.ts
+++ src/orm.config.ts
@@ -1,17 +1,18 @@
 import { Configuration, Options } from "@mikro-orm/core";
-import { SqliteDriver } from "@mikro-orm/sqlite";
+import type { BetterSqliteDriver } from "@mikro-orm/better-sqlite";
 import { TsMorphMetadataProvider } from "@mikro-orm/reflection";
 import * as entities from "./entities";
 
-const config: Options<SqliteDriver> | Configuration<SqliteDriver> = {
-  metadataProvider: TsMorphMetadataProvider,
-  entities: Object.values(entities),
-  dbName: "db.sqlite",
-  type: "sqlite",
-  migrations: {
-    path: "./build/src/migrations",
-    pathTs: "./src/migrations",
-  },
-};
+const config: Options<BetterSqliteDriver> | Configuration<BetterSqliteDriver> =
+  {
+    metadataProvider: TsMorphMetadataProvider,
+    entities: Object.values(entities),
+    dbName: "db.sqlite",
+    type: "better-sqlite",
+    migrations: {
+      path: "./build/src/migrations",
+      pathTs: "./src/migrations",
+    },
+  };
 
 export default config;
--- src/orm.ts
+++ src/orm.ts
@@ -1,8 +1,8 @@
 import { MikroORM } from "@mikro-orm/core";
-import type { SqliteDriver } from "@mikro-orm/sqlite"; // or any other driver package
+import type { BetterSqliteDriver } from "@mikro-orm/better-sqlite";
 import config from "./orm.config";
 
-const ormPromise = MikroORM.init<SqliteDriver>(config);
+const ormPromise = MikroORM.init<BetterSqliteDriver>(config);
 
 export async function getEm() {
   const orm = await ormPromise;
