b771b58e4f2c8d885b6f05996e02955de3d20f0f
mcpower <mcpower@users.noreply.github.com>
2026-04-11 19:15:18 +1000

    Use `satisfies` in config.ts
    The previous way that I did it was a huge hack.

--- src/config.ts
+++ src/config.ts
@@ -1,18 +1,14 @@
 // Type checked config file with TypeScript.
 import * as config from "../config.json";
 
-// oxlint-disable-next-line eslint(no-constant-condition)
-if (false) {
-  function isType<T>(_t: T) {}
-  isType<{
-    token: string;
-    channelId: string;
-    guildId: string;
-    applicationId: string;
-    deleteNoImages: boolean;
-    ormDebug: boolean;
-    discordDebug: boolean;
-  }>(config);
-}
+config satisfies {
+  token: string;
+  channelId: string;
+  guildId: string;
+  applicationId: string;
+  deleteNoImages: boolean;
+  ormDebug: boolean;
+  discordDebug: boolean;
+};
 
 export = config;
